ff4666c4476e4f37102ef6088e7ecee92e4b77bb
shabeeba-i <shabeebaibrahim18@gmail.com>
2026-03-23 10:34:59 +0530

    Add files via upload

---
+++ ExtraAssignment1.py
@@ -0,0 +1,53 @@
+#Print statement
+print("Hope AI")
+
+#Buy input
+instituteName= input("Enter your Institute Name?:")
+
+#Buy inputs
+name= input("Enter your name:")
+age = int(input("Enter your age:"))
+school=input("Enter the school name: ")
+degree=input("Enter your degree:")
+
+# addition
+a=int(input("a="))
+b=int(input("b="))
+add= a+b
+print("add=",add)
+
+#subtraction
+a=int(input("a="))
+b=int(input("b="))
+sub= a-b
+print("sub=",sub)
+
+#Multiplication
+a=int(input("a="))
+b=int(input("b="))
+mul= a*b
+print("mul=",mul)
+
+#Division
+a=int(input("a="))
+b=int(input("b="))
+div= a/b
+print("divison=",div)
+
+# Floor Division
+a=int(input("a="))
+b=int(input("b="))
+FloorDiv= a//b
+print("FloorDivision=",FloorDiv)
+
+#Modulo
+a=int(input("a="))
+b=int(input("b="))
+mod= a%b
+print("Modulo=",mod)
+
+#power
+a=int(input("a="))
+b=int(input("b="))
+power= a**b
+print("Power=",power)
